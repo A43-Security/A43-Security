5a95472640881f104b0d5e23559dac150e58e283
Mohamed <mohamed.sy@compscihigh.org>
2024-06-04 17:34:25 -0400

    temp

--- server/controllers/companiesControllers.js
+++ server/controllers/companiesControllers.js
@@ -1,6 +1,5 @@
 const Companies = require('../db/models/Companies')
 
-
 exports.getAllCompanies = async (req, res) => {
     const companies = await Companies.getAllCompanies();
     res.send(companies);
